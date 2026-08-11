a832fc12437bb437725eaf6db2f9e0e868e9ff4d
madcowGit <yoengsin@hotmail.com>
2026-08-11 15:45:55 +0200

    Remove unnecessary blank line in html_scraper.py

--- custom_components/LUMC_pollentelling/html_scraper.py
+++ custom_components/LUMC_pollentelling/html_scraper.py
@@ -10,7 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 # See the License for the specific language governing permissions and 
 # limitations under the License.
-
 """Helpers for scraping pollen values from the LUMC website."""
 
 import logging
